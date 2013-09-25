e49b17f9fcd66c72257fac0c7c5600d101ed4640
Allen Barker <Allen.L.Barker@gmail.com>
2013-09-25 16:43:25 -0400

    added local pyPdf dirs

---
+++ mstamy2_PyPDF2_7da5545_py3/__init__.py
@@ -0,0 +1,7 @@
+
+# This is a dummy file added by alb to make this dir a module so the import
+# of the subdirectory PyPDF2 is easy using dotted names.  This whole directory
+# was downloaded from
+#    https://github.com/knowah/PyPDF2/
+# and should not be modified.
+
