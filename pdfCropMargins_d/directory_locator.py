a2f80cfdd4594049354cadc53310e0dc10788d43
Allen Barker <Allen.L.Barker@gmail.com>
2014-10-02 14:01:36 -0400

    minor renaming of files

--- pdfCropMargins_d/directory_locator.py
+++
@@ -1,5 +0,0 @@
-
-# This empty file should not be deleted!  It is used by external_programs.py
-# to portably find the name of this directory on an arbitrary system.
-# See that code for the details.
-
