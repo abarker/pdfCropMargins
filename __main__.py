ba6d5e5889ba8cef7ecf6f090a6297983302ab24
Allen Barker <Allen.L.Barker@gmail.com>
2016-08-14 14:55:13 -0400

    started setting up for setuptools

--- __main__.py
+++ __main__.py
@@ -21,8 +21,10 @@ anywhere.
 
 """
 
-import sys
-sys.path.insert(0, "./src")
+import sys, os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(dir_path, "src"))
 
 from pdfCropMargins.pdfCropMargins import main
 
