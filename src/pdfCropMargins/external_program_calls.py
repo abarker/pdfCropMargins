bcb8d295676f82838b53efb57ba8fa4cdfa7cb3a
Allen Barker <Allen.L.Barker@gmail.com>
2017-08-01 17:33:44 -0400

    minor formatting and comments to exernal programs module

--- src/pdfCropMargins/external_program_calls.py
+++ src/pdfCropMargins/external_program_calls.py
@@ -425,7 +425,8 @@ def init_and_test_pdftoppm_executable(prefer_local=False, exit_on_fail=False):
     ignore_called_process_errors = False
 
     global pdftoppm_executable
-    if pdftoppm_executable: return pdftoppm_executable # Has already been set to a path.
+    if pdftoppm_executable:
+        return pdftoppm_executable # Has already been set to a path.
 
     pdftoppm_executable = find_and_test_executable(
                               pdftoppm_executables, ["-v"], "pdftoppm",
@@ -442,6 +443,8 @@ def init_and_test_pdftoppm_executable(prefer_local=False, exit_on_fail=False):
                   "\nthis warning use the '--pdftoppmLocal' (or '-pdl') flag.",
                   file=sys.stderr)
 
+        # TODO: consider using pkgutil.get_data() library function below, to work in
+        # more cases (like zipfiles) to find the packaged executable.
         path = os.path.join(project_src_directory, "pdfCropMargins",
                                                    "pdftoppm_windows_local",
                                                    "xpdfbin_win_3_04")
