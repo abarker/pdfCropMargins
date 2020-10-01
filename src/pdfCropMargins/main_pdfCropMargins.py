4d2a99639fd7e8f91c11d67eded1d4102e8e308b
Allen Barker <Allen.L.Barker@gmail.com>
2020-10-01 19:53:56 -0400

    fix readline not available on Windows

--- src/pdfCropMargins/main_pdfCropMargins.py
+++ src/pdfCropMargins/main_pdfCropMargins.py
@@ -51,7 +51,10 @@ import sys
 import os
 import shutil
 import time
-import readline # Makes prompts go to stdout rather than stderr.
+try:
+    import readline # Makes prompts go to stdout rather than stderr.
+except ImportError: # Not available on Windows.
+    pass
 
 from . import __version__ # Get the version number from the __init__.py file.
 from .manpage_data import cmd_parser, DEFAULT_THRESHOLD_VALUE
